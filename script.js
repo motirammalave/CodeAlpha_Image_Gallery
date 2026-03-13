dbfcce1859a51c2216a94b5a0877e66545c751f0
Motiram Malave <motirammalave@gmail.com>
2026-03-13 12:49:25 +0530

    Updated Image Gallery

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-const images = [
-    "images/photo1.jpg",
-    "images/photo2.jpg",
-    "images/photo3.jpg",
-    "images/photo4.jpg",
-    "images/photo5.jpg"
-];
+const images=[
+"images/photo1.jpg",
+"images/photo2.jpg",
+"images/photo3.jpg",
+"images/photo4.jpg",
+"images/photo5.jpg"
+]
 
-let currentIndex = 0;
+let currentIndex=0
 
-function openLightbox(index) 
-{
-    currentIndex = index;
-    document.getElementById("lightbox").style.display = "flex";
-    document.getElementById("lightbox-img").src = images[currentIndex];
+function openLightbox(index){
+
+currentIndex=index
+
+document.getElementById("lightbox").style.display="flex"
+
+document.getElementById("lightbox-img").src=images[currentIndex]
+
 }
 
-function closeLightbox() 
-{
-    document.getElementById("lightbox").style.display = "none";
+function closeLightbox(){
+
+document.getElementById("lightbox").style.display="none"
+
 }
 
-function changeImage(direction) 
-{
-    currentIndex += direction;
+function changeImage(direction){
 
-    if (currentIndex < 0) 
-    {
-        currentIndex = images.length - 1;
-    }
-    if (currentIndex >= images.length) 
-    {
-        currentIndex = 0;
-    }
+currentIndex+=direction
 
-    document.getElementById("lightbox-img").src = images[currentIndex];
+if(currentIndex<0){
+currentIndex=images.length-1
+}
+
+if(currentIndex>=images.length){
+currentIndex=0
+}
+
+document.getElementById("lightbox-img").src=images[currentIndex]
+
 }
